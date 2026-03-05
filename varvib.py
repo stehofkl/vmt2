9db80c4a9dd869053a031e25b43121948bf26a3f
stehofkl <118087293+stehofkl@users.noreply.github.com>
2026-03-05 22:27:50 +0100

    Create varvib.py

---
+++ varvib.py
@@ -0,0 +1,10 @@
+#This file contains all individual data like mqtt host etc
+mqtt_host         = "mqtt.mqtt.org"
+mqtt_port         = 443
+mqtt_tls          = True
+mqtt_user         = "Yoda"
+mqtt_pass         = "Mickey"
+mqtt_topic        = "my/topic/here/4711"
+mqtt_min_interval = 0.5   # only publish via MQTT if interval >= this value
+interval          = 1.0
+exp_path          = "/home/pi/vib/"
